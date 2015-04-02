2a17db47a952202c551276aeca2f6f1417b727e1
hieunc <hieunc@aoi-sys.vn>
2015-04-02 16:42:50 +0700

    Initial commit

---
+++ app/src/main/java/tk/order_sys/orderapp/MainActivity.java
@@ -0,0 +1,98 @@
+package tk.order_sys.orderapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+
+import tk.order_sys.orderapp.leftmenu.MenuPlaceholderFragment;
+import tk.order_sys.orderapp.leftmenu.NavigationDrawerFragment;
+
+
+public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    String[] MainMenu;
+
+    //Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Get menu forom resources
+        MainMenu = getResources().getStringArray(R.array.array_menu);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, MenuPlaceholderFragment.newInstance(position))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        for (int i = 0; i < MainMenu.length; i++){
+            if(number == i) {
+                mTitle = this.MainMenu[i];
+                break;
+            }
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/tk/order_sys/orderapp/leftmenu/MenuPlaceholderFragment.java
@@ -0,0 +1,65 @@
+package tk.order_sys.orderapp.leftmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tk.order_sys.orderapp.MainActivity;
+import tk.order_sys.orderapp.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MenuPlaceholderFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MenuPlaceholderFragment newInstance(int sectionNumber) {
+        MenuPlaceholderFragment fragment = new MenuPlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MenuPlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView;
+        switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+            case 0:
+                rootView = inflater.inflate(R.layout.menu_home_fragment, container, false);
+                break;
+            case 1:
+                rootView = inflater.inflate(R.layout.menu_category_fragment, container, false);
+                break;
+            case 2:
+                rootView = inflater.inflate(R.layout.menu_category_fragment, container, false);
+                break;
+
+            default:
+                rootView = inflater.inflate(R.layout.menu_home_fragment, container, false);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
